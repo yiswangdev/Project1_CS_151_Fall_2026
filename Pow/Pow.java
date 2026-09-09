b6940c7e5151d042550e6ccf0082863ecc5bf2b0
mfediak <72586020+mfediak@users.noreply.github.com>
2026-09-08 20:58:47 -0700

    added Pow package and problem

---
+++ Pow/Pow.java
@@ -0,0 +1,13 @@
+//Team Members:
+// Eric Wang
+// Maria Fediakina
+// Margarita Sergienko
+// Isaac Guevarra
+package Pow;
+public class Pow {
+    public double myPow(double x, int n) {
+        return;
+        
+    }
+}
+
