a75870a004c0252ec68deba191e4aa20e9a184ed
isaac <tiezic@gmail.com>
2026-09-10 00:00:34 -0700

    add wordsearch solution file

---
+++ word/WordSearch.java
@@ -0,0 +1,19 @@
+/** This comment block should contain your name and the names of your 2 partners
+ * Name:
+ * Isaac Guevarra
+ * 
+ * Partners:
+ * Eric Wang
+ * Maria Fediakina
+ * Margarita Sergienko
+ */
+
+package word;
+
+public class WordSearch {
+    public boolean exist(char[][] board, String word) {
+        
+        return true;
+
+    }
+}
