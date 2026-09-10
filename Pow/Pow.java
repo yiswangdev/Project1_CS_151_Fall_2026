4d20e3d8ea7f72be65d220304999cfdcf7809736
mfediak <72586020+mfediak@users.noreply.github.com>
2026-09-09 20:19:41 -0700

    completed the leetcode problem

--- Pow/Pow.java
+++ Pow/Pow.java
@@ -1,13 +1,21 @@
+package Pow;
 //Team Members:
 // Eric Wang
 // Maria Fediakina
 // Margarita Sergienko
 // Isaac Guevarra
-package Pow;
+
 public class Pow {
     public double myPow(double x, int n) {
-        return;
-        
+        long exp = Math.abs((long) n); 
+        double result = 1;
+        if (x != 1) {
+            for (long i = 0; i < exp; i++) {
+                result *= x;
+            } if (n < 0) {
+                return (1 / result);
+            } 
+        }
+        return result;
     }
 }
-
