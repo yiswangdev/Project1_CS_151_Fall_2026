99e0b0c23eb5b17a04facc95073147e1dabf2f5e
mfediak <72586020+mfediak@users.noreply.github.com>
2026-09-17 16:02:57 -0700

    Added test cases for duplicateFinder and RectangleArea

--- test/RectangleAreaTest.java
+++ test/RectangleAreaTest.java
@@ -1,37 +1,66 @@
 //testers: Margarita, Maria
 
 package test;
+
 import rectangle.RectangleArea;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class RectangleAreaTest {
-    //private class Test{
-        //Margarita Sergienko
-        private RectangleArea areaCalculator = new RectangleArea();
-        @Test
-        public void negativeCoordinates() {
-            assertEquals(19, areaCalculator.computeArea(-3, -3, -1, -1, -2, -2, 2, 2));
-        }
-        
-        @Test
-        public void zeroAreaRectangle() {
-            assertEquals(4, areaCalculator.computeArea(0, 0, 0, 4, 0, 0, 2, 2));
-        }
-        @Test
-        public void adjacent() {
-            assertEquals(8, areaCalculator.computeArea(0, 0, 2, 2, 2, 0, 4, 2));
-        }
-        @Test
-        public void equal() {
-            assertEquals(16, areaCalculator.computeArea(0, 0, 4, 4, 0, 0, 4, 4));
-        }
-        @Test
-        public void overlap() {
-            assertEquals(17, areaCalculator.computeArea(0, 0, 3, 3, 2, 2, 5, 5));
-        }
-    //}
+    // private class Test{
+    // Margarita Sergienko
+    private RectangleArea areaCalculator = new RectangleArea();
+
+    @Test
+    public void negativeCoordinates() {
+        assertEquals(19, areaCalculator.computeArea(-3, -3, -1, -1, -2, -2, 2, 2));
+    }
+
+    @Test
+    public void zeroAreaRectangle() {
+        assertEquals(4, areaCalculator.computeArea(0, 0, 0, 4, 0, 0, 2, 2));
+    }
+
+    @Test
+    public void adjacent() {
+        assertEquals(8, areaCalculator.computeArea(0, 0, 2, 2, 2, 0, 4, 2));
+    }
+
+    @Test
+    public void equal() {
+        assertEquals(16, areaCalculator.computeArea(0, 0, 4, 4, 0, 0, 4, 4));
+    }
+
+    @Test
+    public void overlap() {
+        assertEquals(17, areaCalculator.computeArea(0, 0, 3, 3, 2, 2, 5, 5));
+    }
+
+    // Maria Fediakina
+    @Test
+    public void negativeAndPositive() {
+        assertEquals(100, areaCalculator.computeArea(-5, -5, 5, 5, -2, -2, 2, 2));
+    }
+
+    @Test
+    public void touchingAtCorner() {
+        assertEquals(8, areaCalculator.computeArea(0, 0, 2, 2, 2, 2, 4, 4));
+    }
+
+    @Test
+    public void disjointRectangles() {
+        assertEquals(8, areaCalculator.computeArea(0, 0, 2, 2, 10, 10, 12, 12));
+    }
+
+    @Test
+    public void fullContainment() {
+        assertEquals(8, areaCalculator.computeArea(0, 0, 2, 2, 2, 2, 4, 4));
+    }
+
+    @Test
+    public void partialDiagonalOverlap() {
+        assertEquals(28, areaCalculator.computeArea(0, 0, 4, 4, 2, 2, 6, 6));
+    }
+    // }
 }
-
-
